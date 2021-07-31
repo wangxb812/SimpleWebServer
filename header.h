956f3aae92dae279066246f36907327fef37ef21
wangxb812 <1006089021@qq.com>
2021-08-01 02:18:21 +0800

    update

--- header.h
+++ header.h
@@ -5,5 +5,9 @@
 //Lock
 #include <semaphore.h>
 #include <exception>
+
+#include "list"
+#include "pthreadLock.h"
+#include "sql_connection_pool.h"
 using namespace std;
 
--- threadPool.h
+++ threadPool.h
@@ -1,5 +1,8 @@
 #pragma once
+#include "header.h"
 class threadPool
 {
+    public:
+    threadPool(connection_pool *connnPool,int threadNum=8,int maxRequest = 10000);
 };
 
