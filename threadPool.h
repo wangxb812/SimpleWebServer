0a0eee1dbd6a8025b0f1806c1d2805f9493be2c7
wangxb812 <1006089021@qq.com>
2021-08-02 19:25:21 +0800

    Update threadPool.h

--- threadPool.h
+++ threadPool.h
@@ -2,9 +2,15 @@
 #include "header.h"
 class threadPool
 {
-    public:
+public:
     threadPool(connection_pool *connnPool,int threadNum=8,int maxRequest = 10000);
     ~threadPool();
     bool append(T* request);
+private:
+    static void* worker(void* arg);
+    void run();
+
+    int m_thread_number;
+    int m_max_requests;
 };
 
